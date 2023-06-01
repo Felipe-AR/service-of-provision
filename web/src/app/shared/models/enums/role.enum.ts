8da01c1586970c9112b95cfeb3f1af41b01fe0bf
Felipe Ribeiro <felipe251894@gmail.com>
2023-05-31 23:31:47 -0300

    feat: some model and forms

---
+++ web/src/app/shared/models/enums/role.enum.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  CUSTOMER,
+  SERVICE_PROVIDER,
+  ADMINISTRATOR
+}
---
+++ web/src/app/shared/models/index.ts
@@ -0,0 +1,5 @@
+export * from './customer.model';
+export * from './service-provider.model';
+export * from './user.model';
+export * from './address.model';
+export * from './enums/role.enum';
