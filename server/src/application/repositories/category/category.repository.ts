0c17b460a1535dc5ed515316fb4eb57e92eddca7
Felipe Ribeiro <felipe251894@gmail.com>
2023-06-01 21:12:19 -0300

    fix: changing update argument

--- server/src/application/repositories/category/category.repository.ts
+++ server/src/application/repositories/category/category.repository.ts
@@ -1,9 +1,12 @@
-import { Category } from 'src/application/domain/category/category.entity';
+import {
+  Category,
+  CategoryProperties,
+} from 'src/application/domain/category/category.entity';
 
 export interface CategoryRepository {
   findAll(): Promise<Category[]>;
   findById(id: string): Promise<Category | null>;
   create(category: Category): Promise<Category>;
-  update(id: string, category: Category): Promise<void>;
+  update(id: string, category: CategoryProperties): Promise<void>;
   delete(id: string): Promise<void>;
 }
