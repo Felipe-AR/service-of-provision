c57aa875f8abcd5e5066df338acede16cc53fd44
Felipe Ribeiro <felipe251894@gmail.com>
2023-06-04 19:23:29 -0300

    feat: adding infra modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { HttpModule } from '@infra/http/http.module';
+import { DatabaseModule } from '@infra/database/database.module';
+
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot(), HttpModule, DatabaseModule],
 })
 export class AppModule {}
