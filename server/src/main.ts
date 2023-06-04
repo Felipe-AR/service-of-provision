ba196e3b78c6281319281ff51899c7bdda6932e1
Felipe Ribeiro <felipe251894@gmail.com>
2023-06-04 19:23:10 -0300

    feat: using global pipes to validating

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
-  console.log(process.env.DATABASE_HOST);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(3000);
 }
 bootstrap();
