17cff2429c5e8e4df1df5d958e27e74a1ecf257b
Felipe Ribeiro <felipe251894@gmail.com>
2023-06-11 00:07:44 -0300

    fix: validating e-mail

--- server/src/application/use-cases/user/create-user/create-user.use-case.spec.ts
+++ server/src/application/use-cases/user/create-user/create-user.use-case.spec.ts
@@ -3,14 +3,21 @@ import { InMemoryUserRepository } from '@test/repositories/in-memory-user.reposi
 import { CreateUserUseCase } from './create-user.use-case';
 import { Role } from '@application/domain/user/role.enum';
 import { FindAllUsersUseCase } from '../find-all-users/find-all-users.use-case';
+import { CreateAddressUseCase } from '@application/use-cases/address/create-address/create-address.use-case';
+import { InMemoryAddressRepository } from '@test/repositories/in-memory-address.repository';
 
 describe('Create User', () => {
   it('should be able to create an user', async () => {
     const genericUser = makeUser();
 
-    const repository = new InMemoryUserRepository();
-    const createUserUseCase = new CreateUserUseCase(repository);
-    const findAllUsersUseCase = new FindAllUsersUseCase(repository);
+    const userRepository = new InMemoryUserRepository();
+    const addressRepository = new InMemoryAddressRepository();
+    const createAddressUseCase = new CreateAddressUseCase(addressRepository);
+    const createUserUseCase = new CreateUserUseCase(
+      userRepository,
+      createAddressUseCase,
+    );
+    const findAllUsersUseCase = new FindAllUsersUseCase(userRepository);
 
     const { user: createdUser } = await createUserUseCase.execute({
       email: genericUser.email,
--- server/src/application/use-cases/user/create-user/create-user.use-case.ts
+++ server/src/application/use-cases/user/create-user/create-user.use-case.ts
@@ -1,31 +1,62 @@
 import { Address } from '@application/domain/address/address.entity';
 import { Role } from '@application/domain/user/role.enum';
 import { User } from '@application/domain/user/user.entity';
+import { EmailAlreadyExistsException } from '@application/exceptions/email-already-exists.exception';
 import { UserRepository } from '@application/repositories/user/user.repository';
+import {
+  CreateAddressUseCase,
+  CreateAddressUseCaseRequest,
+} from '@application/use-cases/address/create-address/create-address.use-case';
 import { Injectable } from '@nestjs/common';
 
-interface CreateUserUseCaseRequest {
+export interface CreateUserUseCaseRequest {
   email: string;
   password: string;
   phone: string;
   role: Role;
-  addresses?: Address[];
+  addresses?: Omit<CreateAddressUseCaseRequest, 'userId'>[];
   createdAt?: Date;
   updatedAt?: Date;
 }
+
 interface CreateUserUseCaseResponse {
   user: User;
 }
 
 @Injectable()
 export class CreateUserUseCase {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private createAddressUseCase: CreateAddressUseCase,
+  ) {}
 
   async execute(
     request: CreateUserUseCaseRequest,
   ): Promise<CreateUserUseCaseResponse> {
-    const user = new User(request);
+    const { addresses, ...userRequest } = request;
+    const user = new User(userRequest);
+
+    if (await this.userRepository.findByEmail(user.email)) {
+      throw new EmailAlreadyExistsException();
+    }
+
     const createdUser = await this.userRepository.create(user);
-    return { user: createdUser };
+
+    if (addresses && addresses.length > 0) {
+      createdUser.addresses = await Promise.all(
+        addresses.map(async (address) => {
+          const createdAddress = await this.createAddressUseCase.execute({
+            ...address,
+            userId: createdUser.id,
+          });
+          return createdAddress.address;
+        }),
+      );
+    }
+
+    await this.userRepository.save(createdUser);
+    const udpatedUser = await this.userRepository.find(createdUser.id);
+
+    return { user: udpatedUser };
   }
 }
