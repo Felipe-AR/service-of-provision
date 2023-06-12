a1add41822cadf8d294ae4e9a15ac046aa96b835
Felipe Ribeiro <felipe251894@gmail.com>
2023-06-12 02:49:54 -0300

    fix: changing repository returns

--- server/src/application/repositories/address/address.repository.ts
+++ server/src/application/repositories/address/address.repository.ts
@@ -2,7 +2,7 @@ import { Address } from '@application/domain/address/address.entity';
 
 export abstract class AddressRepository {
   abstract findAll(): Promise<Address[]>;
-  abstract findById(id: string): Promise<Address>;
+  abstract findById(id: string): Promise<Address | null>;
   abstract findAllByUserId(userId: string): Promise<Address[]>;
   abstract create(address: Address): Promise<Address>;
   abstract save(address: Address): Promise<void>;
--- server/src/application/repositories/customer/customer.repository.ts
+++ server/src/application/repositories/customer/customer.repository.ts
@@ -1,7 +1,9 @@
 import { Customer } from '@application/domain/customer/customer.entity';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export abstract class CustomerRepository {
-  abstract find(id: string): Promise<Customer | null>;
+  abstract findByUser(id: string): Promise<Customer | null>;
   abstract findAll(): Promise<Customer[]>;
   abstract create(customer: Customer): Promise<Customer>;
   abstract save(customer: Customer): Promise<void>;
