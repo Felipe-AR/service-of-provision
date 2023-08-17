43b316ff89be9f86533956d8a1f56785b353033c
Felipe Ribeiro <felipe251894@gmail.com>
2023-08-17 17:59:19 -0300

    feat: add dashboard customer notifications page

--- web/src/app/pages/dashboard/dashboard-customer/dashboard-customer-notifications/dashboard-customer-notifications.component.ts
+++ web/src/app/pages/dashboard/dashboard-customer/dashboard-customer-notifications/dashboard-customer-notifications.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PoBreadcrumb } from '@po-ui/ng-components';
+import { PoBreadcrumb, PoListViewAction } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-dashboard-customer-notifications',
@@ -14,4 +14,21 @@ export class DashboardCustomerNotificationsComponent {
       { label: 'Notificações' },
     ],
   };
+
+  public readonly actions: PoListViewAction[] = [
+    { 
+      label: 'Marcar como lido', 
+      icon: 'po-icon-ok', 
+      disabled: true,
+    }
+  ]
+
+  public notifications: any[] = [
+    {
+      id: '4d385379-b863-48eb-a350-a5364c295dec',
+      readAt: new Date(),
+      createdAt: new Date(),
+      description: 'O pedido f87a3f79-fb86-47d2-a562-efcb0f68f504 foi alterado para concluído'
+    }
+  ];
 }
