2f48a69de46b591b64bf3fe6a080bb6701b35079
Felipe Ribeiro <felipe251894@gmail.com>
2023-08-17 18:00:41 -0300

    feat: use po-menu layout

--- web/src/app/pages/dashboard/dashboard-customer/dashboard-customer.module.ts
+++ web/src/app/pages/dashboard/dashboard-customer/dashboard-customer.module.ts
@@ -8,7 +8,29 @@ import { DashboardCustomerServicesComponent } from './dashboard-customer-service
 import { DashboardCustomerNotificationsComponent } from './dashboard-customer-notifications/dashboard-customer-notifications.component';
 import { DashboardCustomerOrdersComponent } from './dashboard-customer-orders/dashboard-customer-orders.component';
 import { DashboardCustomerCategoriesComponent } from './dashboard-customer-categories/dashboard-customer-categories.component';
-import { PoContainerModule, PoPageModule, PoLinkModule } from '@po-ui/ng-components';
+import {
+  PoContainerModule,
+  PoPageModule,
+  PoLinkModule,
+  PoListViewModule,
+  PoInfoModule,
+  PoTableModule,
+  PoModalModule,
+  PoDividerModule,
+  PoDynamicModule,
+  PoAccordionModule,
+  PoWidgetModule,
+  PoTabsModule,
+  PoButtonModule,
+} from '@po-ui/ng-components';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { DashboardCustomerProfilesComponent } from './dashboard-customer-profiles/dashboard-customer-profiles.component';
+import { DashboardCustomerHomeComponent } from './dashboard-customer-home/dashboard-customer-home.component';
+import { PoPageDynamicEditModule, PoPageDynamicTableModule } from '@po-ui/ng-templates';
+import { DashboardCustomerServiceProvidersComponent } from './dashboard-customer-service-providers/dashboard-customer-service-providers.component';
+import { DashboardCustomerServiceProviderDetailsComponent } from './dashboard-customer-service-provider-details/dashboard-customer-service-provider-details.component';
 
 @NgModule({
   declarations: [
@@ -18,8 +40,32 @@ import { PoContainerModule, PoPageModule, PoLinkModule } from '@po-ui/ng-compone
     DashboardCustomerOrdersComponent,
     DashboardCustomerServicesComponent,
     DashboardCustomerSettingsComponent,
+    DashboardCustomerProfilesComponent,
+    DashboardCustomerHomeComponent,
+    DashboardCustomerServiceProvidersComponent,
+    DashboardCustomerServiceProviderDetailsComponent,
   ],
-  imports: [CommonModule, PoContainerModule, PoPageModule, PoLinkModule, RouterModule],
+  imports: [
+    CommonModule,
+    PoContainerModule,
+    PoPageModule,
+    PoLinkModule,
+    PoListViewModule,
+    PoInfoModule,
+    PoTableModule,
+    PoModalModule,
+    PoDividerModule,
+    PoDynamicModule,
+    PoAccordionModule,
+    PoWidgetModule,
+    PoPageDynamicTableModule,
+    PoPageDynamicEditModule,
+    PoTabsModule,
+    PoButtonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule,
+  ],
   exports: [DashboardCustomerComponent],
 })
 export class DashboardCustomerModule {}
