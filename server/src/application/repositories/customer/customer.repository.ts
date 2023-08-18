a97198c8ae5d11cf36f3557d54e6517d4674ee75
Felipe Ribeiro <felipe251894@gmail.com>
2023-08-18 20:31:48 -0300

    feat: add mappers return types

--- server/src/application/repositories/customer/customer.repository.ts
+++ server/src/application/repositories/customer/customer.repository.ts
@@ -1,11 +1,12 @@
 import { Customer } from '@application/domain/customer/customer.entity';
+import { CustomerMapper } from '@application/mappers/customer-mapper';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export abstract class CustomerRepository {
-  abstract findByUser(id: string): Promise<Customer | null>;
-  abstract findAll(): Promise<Customer[]>;
-  abstract create(customer: Customer): Promise<Customer>;
+  abstract findByUser(id: string): Promise<CustomerMapper | null>;
+  abstract findAll(): Promise<CustomerMapper[]>;
+  abstract create(customer: Customer): Promise<CustomerMapper>;
   abstract save(customer: Customer): Promise<void>;
   abstract delete(id: string): Promise<void>;
 }
--- server/src/application/repositories/order/order.repository.ts
+++ server/src/application/repositories/order/order.repository.ts
@@ -1,11 +1,16 @@
 import { Order } from '@application/domain';
+import { OrderMapper } from '@application/mappers/order-mapper';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export abstract class OrderRepository {
-  abstract find(id: string): Promise<Order | null>;
-  abstract findAll(): Promise<Order[]>;
-  abstract create(order: Order): Promise<Order>;
+  abstract find(id: string): Promise<OrderMapper | null>;
+  abstract findAll(): Promise<OrderMapper[]>;
+  abstract findAllByCustomer(customerId: string): Promise<OrderMapper[]>;
+  abstract findAllByServiceProvider(
+    serviceProviderId: string,
+  ): Promise<OrderMapper[]>;
+  abstract create(order: Order): Promise<OrderMapper>;
   abstract save(order: Order): Promise<void>;
   abstract delete(id: string): Promise<void>;
 }
