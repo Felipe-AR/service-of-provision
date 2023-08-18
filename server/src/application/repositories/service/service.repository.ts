507547008421b5b5d47145eb757169766cc0c538
Felipe Ribeiro <felipe251894@gmail.com>
2023-08-18 20:28:01 -0300

    feat: add find all services by ids method

--- server/src/application/repositories/service/service.repository.ts
+++ server/src/application/repositories/service/service.repository.ts
@@ -6,6 +6,7 @@ import { Service } from '@application/domain/service/service.entity';
 export abstract class ServiceRepository {
   abstract find(id: string): Promise<Service | null>;
   abstract findAll(): Promise<Service[]>;
+  abstract findAllByIdIn(servicesIds: string[]): Promise<Service[]>;
   abstract create(service: Service): Promise<Service>;
   abstract save(service: Service): Promise<void>;
   abstract delete(id: string): Promise<void>;
