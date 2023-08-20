7193b58a1ac989ce03a845a989f649dcf8ec022f
Felipe Ribeiro <felipe251894@gmail.com>
2023-08-20 03:36:58 -0300

    feat: add analytics use cases

--- server/src/application/use-cases/analytics/customer-analytics-count/customer-analytics-count.use-case.ts
+++ server/src/application/use-cases/analytics/customer-analytics-count/customer-analytics-count.use-case.ts
@@ -1,1 +1,58 @@
-export class CustomerAnalyticsCountUseCase {}
+import { Injectable } from '@nestjs/common';
+
+import { OrderRepository } from '@application/repositories/order/order.repository';
+import { FindCustomerUseCase } from '@application/use-cases/customer/find-customer/find-customer.use-case';
+import { OrderStatus } from '@application/domain';
+
+export interface CustomerAnalyticsCountUseCaseRequest {
+  userId: string;
+}
+
+export interface CustomerAnalyticsCountUseCaseResponse {
+  requestedServices: number;
+  ongoingServices: number;
+  finishedServices: number;
+}
+
+@Injectable()
+export class CustomerAnalyticsCountUseCase {
+  constructor(
+    private findCustomerUseCase: FindCustomerUseCase,
+    private orderRepository: OrderRepository,
+  ) {}
+
+  async execute(
+    request: CustomerAnalyticsCountUseCaseRequest,
+  ): Promise<CustomerAnalyticsCountUseCaseResponse> {
+    const { userId } = request;
+    const { customer } = await this.findCustomerUseCase.execute({ userId });
+
+    const orders = await this.orderRepository.findAllByCustomer(
+      customer.user.id,
+    );
+
+    const counts = orders.reduce(
+      (previousOrder, currentOrder) => {
+        const currentStatus = currentOrder.status;
+
+        if (currentStatus in previousOrder) {
+          previousOrder[currentStatus]++;
+        }
+
+        return previousOrder;
+      },
+      {
+        [OrderStatus.CREATED]: 0,
+        [OrderStatus.ACCEPTED]: 0,
+        [OrderStatus.DENIED]: 0,
+        [OrderStatus.COMPLETED]: 0,
+      },
+    );
+
+    return {
+      requestedServices: counts.CREATED,
+      ongoingServices: counts.ACCEPTED,
+      finishedServices: counts.COMPLETED,
+    };
+  }
+}
--- server/src/application/use-cases/analytics/service-provider-analytics-count/service-provider-analytics-count.use-case.ts
+++ server/src/application/use-cases/analytics/service-provider-analytics-count/service-provider-analytics-count.use-case.ts
@@ -1,1 +1,60 @@
-export class ServiceProviderAnalyticsCountUseCase {}
+import { Injectable } from '@nestjs/common';
+
+import { OrderRepository } from '@application/repositories/order/order.repository';
+import { FindServiceProviderUseCase } from '@application/use-cases/service-provider/find-service-provider/find-service-provider.use-case';
+import { OrderStatus } from '@application/domain';
+
+export interface ServiceProviderAnalyticsCountUseCaseRequest {
+  userId: string;
+}
+
+export interface ServiceProviderAnalyticsCountUseCaseResponse {
+  requestedServices: number;
+  ongoingServices: number;
+  finishedServices: number;
+}
+
+@Injectable()
+export class ServiceProviderAnalyticsCountUseCase {
+  constructor(
+    private findServiceProviderUseCase: FindServiceProviderUseCase,
+    private orderRepository: OrderRepository,
+  ) {}
+
+  async execute(
+    request: ServiceProviderAnalyticsCountUseCaseRequest,
+  ): Promise<ServiceProviderAnalyticsCountUseCaseResponse> {
+    const { userId } = request;
+    const { serviceProvider } = await this.findServiceProviderUseCase.execute({
+      userId,
+    });
+
+    const orders = await this.orderRepository.findAllByServiceProvider(
+      serviceProvider.user.id,
+    );
+
+    const counts = orders.reduce(
+      (previousOrder, currentOrder) => {
+        const currentStatus = currentOrder.status;
+
+        if (currentStatus in previousOrder) {
+          previousOrder[currentStatus]++;
+        }
+
+        return previousOrder;
+      },
+      {
+        [OrderStatus.CREATED]: 0,
+        [OrderStatus.ACCEPTED]: 0,
+        [OrderStatus.DENIED]: 0,
+        [OrderStatus.COMPLETED]: 0,
+      },
+    );
+
+    return {
+      requestedServices: counts.CREATED,
+      ongoingServices: counts.ACCEPTED,
+      finishedServices: counts.COMPLETED,
+    };
+  }
+}
