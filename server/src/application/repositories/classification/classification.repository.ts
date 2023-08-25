7d6460cd215dec4b46196f0593cccff9fa6b0761
Felipe Ribeiro <felipe251894@gmail.com>
2023-08-25 07:39:12 -0300

    feat: add method into repository

--- server/src/application/repositories/classification/classification.repository.ts
+++ server/src/application/repositories/classification/classification.repository.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export abstract class ClassificationRepository {
   abstract find(id: string): Promise<Classification | null>;
+  abstract findAllByOrder(orderId: string): Promise<Classification[]>;
   abstract findAllByUserRated(userId: string): Promise<Classification[]>;
   abstract create(classification: Classification): Promise<Classification>;
   abstract save(classification: Classification): Promise<void>;
