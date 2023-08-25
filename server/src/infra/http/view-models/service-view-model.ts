8402873dc4efdd338f192134e668c3ae38c21ccc
Felipe Ribeiro <felipe251894@gmail.com>
2023-08-25 07:47:15 -0300

    feat: add dto with relations return

--- server/src/infra/http/view-models/service-view-model.ts
+++ server/src/infra/http/view-models/service-view-model.ts
@@ -1,10 +1,21 @@
-import { Service } from '@application/domain';
+import { Category, Service } from '@application/domain';
+import { ServiceMapper } from '@application/mappers';
 import { ServiceStatus } from '@prisma/client';
+import { CategoryDTO, CategoryViewModel } from './category-view-model';
 
 export interface ServiceDTO {
   id: string;
   serviceProviderId: string;
   categoryId: string;
+  name: string;
+  price: number;
+  status: ServiceStatus;
+}
+
+export interface ServiceMapperDTO {
+  id: string;
+  serviceProviderId: string;
+  category: CategoryDTO;
   name: string;
   price: number;
   status: ServiceStatus;
@@ -16,6 +27,17 @@ export class ServiceViewModel {
       id: service.id,
       serviceProviderId: service.serviceProviderId,
       categoryId: service.categoryId,
+      name: service.name,
+      price: service.price,
+      status: service.status,
+    };
+  }
+
+  static mapperToHTTP(service: ServiceMapper): ServiceMapperDTO {
+    return {
+      id: service.id,
+      serviceProviderId: service.serviceProviderId,
+      category: CategoryViewModel.toHTTP(service.category),
       name: service.name,
       price: service.price,
       status: service.status,
