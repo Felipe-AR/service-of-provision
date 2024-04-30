7aaf9d4c8615886277471f5f281ecf333ff065d4
Felipe Ribeiro <felipe251894@gmail.com>
2024-04-30 09:42:16 -0300

    fix: remove service provider id

--- server/src/infra/http/forms/create-service.form.ts
+++ server/src/infra/http/forms/create-service.form.ts
@@ -3,10 +3,6 @@ import { IsNotEmpty, IsUUID, IsString, IsNumber } from 'class-validator';
 export class CreateServiceForm {
   @IsNotEmpty()
   @IsUUID()
-  serviceProviderId: string;
-
-  @IsNotEmpty()
-  @IsUUID()
   categoryId: string;
 
   @IsString()
